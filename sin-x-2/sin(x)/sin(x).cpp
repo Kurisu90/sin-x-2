be1e86fc8981b1c414e50dce5fb58045de8b8f81
Makise Kurisu <3887277988@qq.com>
2024-09-27 08:24:12 +0800

    Add files via upload

---
+++ sin-x-2/sin(x)/sin(x).cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main() {
+	double y;
+	int x, m;
+	for (y = 1; y >= 0; y -= 0.1) {
+		m = asin(y) * 10;
+		for (x = 1; x < m; x += 1)
+			printf(" ");
+		printf("*");
+		for (; x < 29 - m; x += 1)
+			printf(" ");
+		printf("*\n");
+	}
+	double Y;
+	int X, M;
+	for (Y = 0; Y >= -1; Y -= 0.1) {
+		M = asin(Y) * 10; //注意M是负数!!!!!
+		for (X = 1; X < 31-M; X += 1)
+			printf(" ");
+		printf("*");
+		for (; X <M+61; X += 1)
+			printf(" ");
+		printf("*\n");
+	}
+
+}
